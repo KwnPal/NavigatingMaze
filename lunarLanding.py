fb95206d0a07fada26e4e43cb6799eca5eb4b152
Dounas P <ntinospalegas@gmail.com>
2024-10-03 14:59:41 +0300

    misc

--- lunarLanding.py
+++ lunarLanding.py
@@ -8,8 +8,8 @@ from stable_baselines3.common.evaluation import evaluate_policy
 env = gym.make("LunarLander-v2")
 
 # Instantiate the agent
-model = DQN("MlpPolicy",env,learning_rate=0.0005, verbose=1)
+model = DQN("MlpPolicy",env, verbose=1)
 # Train the agent and display a progress bar
-model.learn(total_timesteps=int(1000000), progress_bar=True)
+model.learn(total_timesteps=int(200000), progress_bar=True)
 # Save the agent
 model.save("dqn_lunar2")
