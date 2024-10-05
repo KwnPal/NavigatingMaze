ae6c5bc0b4755711bf97d9841aea6df7e4cb9eda
DounasP <ntinospalegas@gmail.com>
2024-10-05 20:27:18 +0300

    multiprocessing

--- TestClass.py
+++ TestClass.py
@@ -3,6 +3,7 @@ from envGymWrapper import GymEnvWrapper
 from stable_baselines3.common.env_checker import check_env
 from stable_baselines3.common.vec_env import DummyVecEnv, SubprocVecEnv
 from stable_baselines3 import DQN
+from stable_baselines3 import PPO
 # 0-->left 1-->right 2->down 3 -->up 
 
 
@@ -13,17 +14,19 @@ def main():
     
     #check_env(env,warn=True)
     
-    num_envs = 2  # Number of parallel environments
+    num_envs = 4  # Number of parallel environments
     env = SubprocVecEnv([make_env(url) for _ in range(num_envs)])
 
 
-    model = DQN("MlpPolicy",env,verbose=2)
-    model.learn(total_timesteps=200000, progress_bar=True)
+    model = DQN("MlpPolicy",env,verbose=1)
+    model.learn(total_timesteps=25000,progress_bar=True)
     model.save("DQN")
 
 
 def make_env(url):
-    return GymEnvWrapper(url)
+    def _init():
+        return GymEnvWrapper(url)  # Return a new instance of GymEnvWrapper
+    return _init
 
 
 # print("done: ",done,"| info: ",info,"| observation: ",observation,"| reward: ",reward,"| truncated:",truncated)
